db2cf245aae3181c999a54e089620292b4fbcc1c
tonycor <corriveau.anthony@gmail.com>
2019-11-28 21:43:32 -0500

    major refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,84 @@
-// Gulpfile
+/***
+ * Author : Anthony Corriveau
+ * Version : 1.0
+ * Gulp Version : 4.0
+ * Note :
+ *  ...
+ *
+ * Usage :
+ * 		cmd    		< optional >
+ * 		----------------------------------
+ * 		gulp 		< --env production/development >
+ * 		gulp watch  < --env production/development >
+ * 		gulp sass 	< --env production/development >
+ * 		gulp js 	< --env production/development >
+ * 		gulp clean
+ *
+ * @type {Gulp}
+ */
 
+// Depedencies instances
 var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     notify = require('gulp-notify'),
     concat = require('gulp-concat'),
     sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    watch = require('gulp-watch'),
-    cssnano = require('gulp-cssnano');
-    uglify = require('gulp-uglify');
-    mergestreams = require('merge-stream');
-    gutil = require('gulp-util');
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('autoprefixer'),
+    csslint = require('gulp-csslint'),
+    cssnano = require('cssnano'),
+    uglify = require('gulp-uglify'),
+    environments = require('gulp-environments'),
+    postcss = require('gulp-postcss'),
+    del = require('del');
 
+// Paths vars
+var pathBowerLibs = './bower_components',
+    pathNodeLibs =  './node_modules',
+    pathAssets  =   './assets',
+    pathSrcBase =   './src',
+    pathSrcSASS =   './src/sass',
+    pathSrcJS =     './src/js',
+    pathDestBase =  './assets/generated',
+    pathDestCSS =   './assets/generated/css',
+    pathDestLibsJS = './assets/libs/js',
+    pathDestJS =    './assets/generated/js';
+
+// Gulpfile
 var jsSources = ['scripts/*.js'],
     sassSources = ['styles/*.scss'],
     htmlSources = ['**/*.html'],
     outputDir = 'assets';
 
-    /*
-gulp.task('copy', async function() {
-    gulp.src('index.php').pipe(gulp.dest('assets'));
-});*/
 
-gulp.task('log', async function() {
-    gutil.log('== My Log Task ==')
-});
+// Environment vars
+var development = environments.development;
+var production = environments.production;
+
+if(!environments.current()){ environments.current(production); }
+
+// Files to concat
+var files = {
+    scripts: {
+        loose: [
+            /*pathBowerLibs + '/slick-carousel/slick/slick.js',
+            pathBowerLibs + '/matchHeight/jquery.matchHeight.js',
+            pathBowerLibs+'/aos/dist/aos.js'*/
+        ],
+        bundled: [
+            pathSrcJS + '/main.js'
+        ]
+    },
+    sass: [
+        pathBowerLibs+'/slick-carousel/slick/slick-theme.css',
+        pathBowerLibs+'/slick-carousel/slick/slick.css',
+        pathNodeLibs+'/bootstrap-4-grid/css/grid.css',
+        pathSrcSASS+'/main.scss',
+    ]
+};
 
 // Autoprefixer browser support options
 var autoprefixerOptions = {
-    browsers: ['last 8 versions', '> 1%', 'IE 10'],
     grid: true
 };
 
@@ -44,56 +94,60 @@ var onError = function (err) {
     this.emit('end');
 };
 
-gulp.task('sass', async function() {
-    gulp.src(sassSources)
-        .pipe(plumber({errorHandler: onError}))
-        .pipe(sass({style: 'expanded'}))
-        .pipe(sass({errLogToConsole:true}).on('error', sass.logError)) // Transpile Sass into Css
-       /* .pipe(concat('theme.css')) */// merge files
-        .pipe(autoprefixer()) //vendors autoprefix
-       /* .pipe(cssnano())*/ //minify
-        .pipe(gulp.dest(outputDir))
-    /*
+// Build Sass files and autoprefix theme
+function sassFiles(cb) {
+    return gulp.src(files.sass)
         .pipe(plumber({errorHandler: onError})) // fix issue with Node Streams piping.
         .pipe(development(sourcemaps.init())) //init maps
-        //.pipe(development(scsslint())) //css linting
         .pipe(sass({errLogToConsole:true}).on('error', sass.logError)) // Transpile Sass into Css
+        .pipe(csslint()) //css linting
         .pipe(concat('theme.css')) // merge files
-        .pipe(autoprefixer(autoprefixerOptions)) //vendors autoprefix
-        .pipe(production(cssnano())) //minify
+        .pipe(development(postcss([autoprefixer(autoprefixerOptions)])))
+        .pipe(production(postcss([autoprefixer(autoprefixerOptions), cssnano()])))
         .pipe(development(sourcemaps.write('.'))) // Create sourcemaps
-        .pipe(gulp.dest(pathDestCSS)); // Place generated file into the right directory
- */
+        .pipe(gulp.dest(pathDestCSS))
+    cb();
+}
+
+// Concat and uglify scripts
+function scriptsFiles(cb) {
+    return gulp.src(files.scripts.bundled)
+        .pipe(plumber({errorHandler: onError})) // fix issue with Node Streams piping.
+        .pipe(development(sourcemaps.init())) //init maps
+        .pipe(concat('theme.js')) // merge files
+        .pipe(production(uglify())) //Minify javascript
+        .pipe(development(sourcemaps.write('.'))) // Create sourcemaps
+        .pipe(gulp.dest(pathDestJS)) // Place generated file into the right directory
+    cb();
+}
+
+// Move all javascripts libs specified into project files to be used by requirejs
+function scriptsFilesLoose(cb) {
+    return gulp.src(files.scripts.loose)
+        .pipe(plumber({errorHandler: onError}))
+        .pipe(gulp.dest(pathDestLibsJS))
+    cb();
+}
 
 
-});
+//Clean generated folder
+function clean() {
+    return del([pathDestBase]);
+}
+//Files to watch
+function watchFiles() {
+    gulp.watch(['src/*'], gulp.parallel(sassFiles,scriptsFiles,/*scriptsFilesLoose*/));
+}
 
-gulp.task('js', async function() {
-    gulp.src(jsSources)
-        .pipe(uglify())
-        .pipe(concat('script.js'))
-        .pipe(gulp.dest(outputDir))
+// define complex tasks
+const build = gulp.series(clean, gulp.parallel(sassFiles,scriptsFiles,/*scriptsFilesLoose*/));
+const watch = gulp.parallel(watchFiles);
 
-    /*
-        	// Bundle all specified files into a merged an uglified one.
-	var bundledScripts = gulp.src(files.scripts.bundled) // gather sources
-		.pipe(plumber({errorHandler: onError})) // fix issue with Node Streams piping.
-		.pipe(development(sourcemaps.init())) //init maps
-		.pipe(concat('theme.js')) // merge files
-		.pipe(production(uglify())) //Minify javascript
-		.pipe(development(sourcemaps.write('.'))) // Create sourcemaps
-		.pipe(gulp.dest(pathDestJS)); // Place generated file into the right directory
-
-	// Move all javascripts libs specified into project files to be used by requirejs
-	var looseScripts = gulp.src(files.scripts.loose)
-		.pipe(plumber({errorHandler: onError}))
-		.pipe(gulp.dest(pathDestLibsJS));
-
-	return mergestreams(bundledScripts,looseScripts);
-   */
-});
-
-gulp.task('watch', function() {
-   // gulp.watch(sassSources, ['sass']);
-    gulp.watch(['styles/*','scripts/*'], gulp.series('sass',"js"));
-});
+// export tasks
+exports.sass = sassFiles;
+exports.js = scriptsFiles;
+exports.jsloose = scriptsFilesLoose;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
