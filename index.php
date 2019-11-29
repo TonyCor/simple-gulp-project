41a543efe2f7b02a6219a914fe6c98c67f157982
tonycor <corriveau.anthony@gmail.com>
2019-11-28 20:57:41 -0500

    add index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr-CA">
+<head>
+    <meta charset="utf-8">
+    <title>Title</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="icon" type="image/png" href="favicon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+
+</body>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="assets/script.js"></script>
+<script>
+    AOS.init({
+        duration: 1200,
+        once: true,
+    })
+</script>
+</html>
