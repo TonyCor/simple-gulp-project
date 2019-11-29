ed5913c012514b87ed8db2d1295540a6d59726b4
tonycor <corriveau.anthony@gmail.com>
2019-11-28 21:57:44 -0500

    add stylesheet link

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta name="author" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="stylesheet" href="assets/generated/css/theme.css">
 </head>
 
 <body>
@@ -15,6 +16,6 @@
 </body>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="assets/script.js"></script>
+<script src="assets/generated/js/theme.js"></script>
 
 </html>
